fffbddc521e43a588b0855fafe022245d64307d0
Едисеев Максим Иванович <ediseev_mi@albank.ru>
2025-04-08 18:31:36 +0900

    dsads

--- src/BusinessReg.jsx
+++ src/BusinessReg.jsx
@@ -510,7 +510,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import ChatBot from './components/ChatBot';
+// import ChatBot from './components/ChatBot';
 
 const BusinessRegistration = () => {
 	const token = localStorage.getItem('token');
