8c239d8f820e955427ec2d12dbbc33d8d98df313
Renat <ShiruiSan@yandex.ru>
2025-04-09 18:56:52 +0900

    test

--- src/components/Layout/MainContent.jsx
+++ src/components/Layout/MainContent.jsx
@@ -912,7 +912,7 @@ export const MainContent = memo(({
 		<main
 			ref={mainRef}
 			onClick={() => setIsSwiped(true)}
-			className={`md:fixed rounded-br-2xl pr-4 md:pt-2 md:px-6 md:left-0 w-screen md:w-[calc(100vw-17rem)] h-[calc(100vh-136px)] overflow-y-auto overflow-x-hidden transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'
+			className={`md:fixed rounded-br-2xl pr-4 md:pt-2 md:px-6 md:left-0 w-screen md:w-[calc(100vw-17rem)] h-[calc(100vh-136px)] overflow-y-auto overflow-x-hidden transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-full' : 'translate-x-0'
 				} ${!isSidebarOpen ? 'md:hidden' : 'md:block'} md:translate-x-0 z-10 ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'
 				}`}
 		>
